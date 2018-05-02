e2f6434ce803c8cbbcd13c8a919b5b486547acb9
QuangNguyen <hikari0901@gmail.com>
2018-05-02 18:37:14 +0700

    aaa

--- vidu1/1.js
+++ vidu1/1.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	$('.dangky').animate({opacity: 0,marginLeft:-100});
+	//$('.dangky').animate({opacity: 0,marginLeft:-100})
+	$('.nutso2').click(function(){
+		//$('.dangky').animate({opacity: 0,marginLeft:-100});
+		$('.dangky').hide();
+		//$('.dangnhap').animate({opacity: 1,marginLeft:0})
+		$('.dangnhap').show();
+	});
+	$('.nutso1').click(function(){
+		$('.dangnhap').hide();
+		//$('.dangnhap').animate({opacity: 0,marginLeft:-100});
+		//$('.dangky').animate({opacity: 1,marginLeft:0});
+		$('.dangky').show();
+	});
 });
