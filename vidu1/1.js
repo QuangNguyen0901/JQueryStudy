53e06d4f205d372a4be0e14a1b81a73f1c61cf91
quangnguyen <hikari0901@gmail.com>
2018-05-12 10:12:09 +0700

    conflict

--- vidu1/1.js
+++ vidu1/1.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-	$('.dangky').animate({opacity: 0,marginLeft:-100});
+$(function() {
+	$('.nutso1').click(function(event) {
+		/* Act on the event */
+		$('.dangnhap').animate({opacity: 0});
+		$('.dangky').animate({opacity: 1});
+	});
+
+	$('.nutso2').click(function(event) {
+		/* Act on the event */
+		$('.dangky').animate({opacity: 0});
+		$('.dangnhap').animate({opacity: 1});
+	});
 });
