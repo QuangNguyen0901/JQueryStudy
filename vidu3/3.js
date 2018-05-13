79b9eb24825d224da1d379844c681c578d820cf8
quangnguyen <hikari0901@gmail.com>
2018-05-13 17:25:16 +0700

    done vidu3

--- vidu3/3.js
+++ vidu3/3.js
@@ -1,8 +1,32 @@
-$( document ).ready(function() {
-	
-	$('.menutren ul li a').on('click', function(event) {
+$(function() {
+	$('.menutren ul li:nth-child(1) a').on('click',function(event) {
+		event.preventDefault();
 		/* Act on the event */
-		// console.log('chay chua ha cac ban ');
-		$('body').animate({scrollTop:1500},500);
+		$('html,body').animate({scrollTop: $('.chapter1').offset().top},1000,"easeOutElastic"); // $('body') -> $('html,body')
 	});
+
+	$('.menutren ul li:nth-child(2) a').on('click',function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$('html,body').animate({scrollTop: $('.chapter2').offset().top},1000,"easeInOutExpo"); // $('body') -> $('html,body')
+	});
+
+	$('.menutren ul li:nth-child(3) a').on('click',function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$('html,body').animate({scrollTop: $('.chapter3').offset().top},1000,"easeInOutBack"); // $('body') -> $('html,body')
+	});
+
+	$('.menutren ul li:nth-child(4) a').on('click',function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$('html,body').animate({scrollTop: $('.chapter4').offset().top},1000); // $('body') -> $('html,body')
+	});
+
+	$('.nutlentren').on('click',function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$('html,body').animate({scrollTop: 0},2000,"easeInOutExpo"); // $('body') -> $('html,body')
+	});
+
 });
