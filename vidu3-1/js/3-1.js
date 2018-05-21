b4a6447ca19a2cba4d1339b325334ecbc1411b42
QuangNguyen <hikari0901@gmail.com>
2018-05-21 17:19:55 +0700

    complete bai3

--- vidu3-1/js/3-1.js
+++ vidu3-1/js/3-1.js
@@ -2,7 +2,14 @@ $(function () {
 	$('.tdkhoi').click(function(event) {
 		/* Act on the event */
 		$(this).toggleClass('xanh');
-	});;
 
+		$(this).next().slideToggle(400);
 
-})
+		$('html,body').animate({scrollTop: $(this).offset().top});
+	});
+
+	// code cho phan phong to anh 
+	$('.vaicaianh a').fancybox(); 
+
+	$('.ndkhoi').slideUp();
+});
