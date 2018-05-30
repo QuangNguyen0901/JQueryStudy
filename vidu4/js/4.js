ca53c77a79dc850255f131c93e99482c8a68624a
QuangNguyen <hikari0901@gmail.com>
2018-05-30 18:50:31 +0700

    aaa

--- vidu4/js/4.js
+++ vidu4/js/4.js
@@ -1,4 +1,12 @@
 $(function() {
+	$('.tinloadthem').slideUp(0);
 	// TweenMax.from($('.tinto'),1,{top:100,opacity:0})
-	TweenMax.staggerFrom($('.tinto'),1,{top:100,opacity:0},0.4)
+	TweenMax.staggerFrom($('.tinbandau .tinto'),1,{top:100,opacity:0},0.4);
+
+	$('.btnloadthem').click(function(event) {
+		/* Act on the event */
+		$('.tinloadthem').slideDown(0);
+		TweenMax.staggerFrom($('.tinloadthem .tinto'),0.5,{top:100,opacity:0},0.1);
+	});
+
 });
